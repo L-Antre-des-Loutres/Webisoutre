d0056ac973a99d19485a0b27dd1d77a41eb64c8e
Perodeau <perod.matheo@gmail.com>
2024-10-12 20:05:28 +0000

    Correction de la variable

--- resources/views/components/liste_serveur.blade.php
+++ resources/views/components/liste_serveur.blade.php
@@ -1,13 +1,13 @@
 <div id="liste_serveur">
 
     @php
-        // Vérifie si la variable $jeu n'est pas définie
-        if (!isset($serveur)) {
-            $listeConfig = []; // Initialise $liste
+        // Vérifie si la variable $listeConfig n'est pas définie
+        if (!isset($listeConfig)) {
+            $listeConfig = [];
         }
 
         /* Exemple de variable de configuration pour l'affichage dans le tableau */
-        //$serveur = ["id_serv" => true, "jeu" => true, "nom_serv" => true, "modpack" => true, "modpack_url" => true, "embedColor" => true, "nom_monde" => true, "version_serv" => true, "path_serv" => true, "start_script" => true, "administrateur" => true, "actif" => true, "nb_joueurs" => true, "players" => true, "online" => true];
+        //$listeConfig = ["id_serv" => true, "jeu" => true, "nom_serv" => true, "modpack" => true, "modpack_url" => true, "embedColor" => true, "nom_monde" => true, "version_serv" => true, "path_serv" => true, "start_script" => true, "administrateur" => true, "actif" => true, "nb_joueurs" => true, "players" => true, "online" => true];
 
     @endphp
 
