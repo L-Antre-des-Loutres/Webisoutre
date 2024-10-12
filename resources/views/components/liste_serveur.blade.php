0fc885fbcf60eb25906a61e238c292a25d05ab0e
Perodeau <perod.matheo@gmail.com>
2024-10-12 20:04:46 +0000

    Configuration par défaut de la liste

--- resources/views/components/liste_serveur.blade.php
+++ resources/views/components/liste_serveur.blade.php
@@ -1,3 +1,17 @@
 <div id="liste_serveur">
-    
+
+    @php
+        // Vérifie si la variable $jeu n'est pas définie
+        if (!isset($serveur)) {
+            $listeConfig = []; // Initialise $liste
+        }
+
+        /* Exemple de variable de configuration pour l'affichage dans le tableau */
+        //$serveur = ["id_serv" => true, "jeu" => true, "nom_serv" => true, "modpack" => true, "modpack_url" => true, "embedColor" => true, "nom_monde" => true, "version_serv" => true, "path_serv" => true, "start_script" => true, "administrateur" => true, "actif" => true, "nb_joueurs" => true, "players" => true, "online" => true];
+
+    @endphp
+
+
+
+
 </div>
