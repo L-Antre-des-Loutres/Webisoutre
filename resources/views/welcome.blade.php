5edeb814a13ff5062db787ffebef5fdb39d1223f
Perodeau <perod.matheo@gmail.com>
2024-10-13 16:45:49 +0000

    Composant Liste serveur configurable via variable et disponible dans l'ensemble des fichiers

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,4 +6,29 @@
 @section('content')
     <h1>Bienvenue sur la page d'accueil</h1>
     <p>Ceci est le contenu de la page d'accueil.</p>
+
+
+    <p>Tableau des serveurs</p>
+    @php
+        $jeu = 'Minecraft';
+        $listeConfig = [
+            'id_serv' => false,
+            'jeu' => true,
+            'nom_serv' => true,
+            'modpack' => true,
+            'modpack_url' => true,
+            'embedColor' => false,
+            'nom_monde' => true,
+            'version_serv' => true,
+            'path_serv' => true,
+            'start_script' => true,
+            'administrateur' => false,
+            'actif' => true,
+            'nb_joueurs' => true,
+            'players' => true,
+            'online' => true,
+        ];
+    @endphp
+    @include('components.liste_serveur')
+
 @endsection
